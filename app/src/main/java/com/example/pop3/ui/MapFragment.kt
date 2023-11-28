abd2dc1e3c633369ccf01722fbdb20f91c6589df
juanpalegre <juan.pablo.alegre@it4w.net>
2023-11-28 09:45:56 -0300

    map fragment integrated

--- app/src/main/java/com/example/pop3/ui/MapFragment.kt
+++ app/src/main/java/com/example/pop3/ui/MapFragment.kt
@@ -5,14 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import com.example.pop3.R
 import com.example.pop3.databinding.FragmentMapBinding
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 
-class MapFragment : Fragment(R.layout.fragment_map) {
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
 
     private lateinit var binding: FragmentMapBinding
+    private lateinit var map: GoogleMap
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMapBinding.bind(view)
+        createMapFragment()
+    }
+
+    private fun createMapFragment() {
+        val mapFragment = childFragmentManager
+            .findFragmentById(R.id.fragmentMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
     }
 }
