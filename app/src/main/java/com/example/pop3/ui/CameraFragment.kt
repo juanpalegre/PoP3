9deef40798eeada2815d739209a16871fc9c5170
juanpalegre <juan.pablo.alegre@it4w.net>
2023-12-01 15:59:04 -0300

    CameraFragment logic

--- app/src/main/java/com/example/pop3/ui/CameraFragment.kt
+++ app/src/main/java/com/example/pop3/ui/CameraFragment.kt
@@ -1,28 +1,113 @@
 package com.example.pop3.ui
 
+import android.app.Activity
+import android.app.Activity.RESULT_OK
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.pop3.R
 import com.example.pop3.databinding.FragmentCameraBinding
-import com.google.zxing.integration.android.IntentIntegrator
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.OnSuccessListener
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 
 class CameraFragment : Fragment(R.layout.fragment_camera) {
 
     private lateinit var binding: FragmentCameraBinding
-
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val REQUEST_LOCATION_PERMISSION = 1
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentCameraBinding.bind(view)
+        Log.d("CameraFragment", "onViewCreated")
 
+        // Solicitar permisos de ubicación
+        if (ContextCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // Si ya se concedieron los permisos, obtén la ubicación
+            // Puedes usar FusedLocationProviderClient para obtener la ubicación actual
+            val fusedLocationClient: FusedLocationProviderClient =
+                LocationServices.getFusedLocationProviderClient(requireContext())
+
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener(requireActivity(), OnSuccessListener { location ->
+                    // location es la última ubicación conocida
+                    if (location != null) {
+                        // Actualizar la interfaz con la ubicación obtenida
+                        updateInterfaceWithLocationData(location.latitude, location.longitude)
+                    }
+                })
+        } else {
+            // Si no se concedieron, solicitar permisos
+            requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        }
+
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException){
+            Toast.makeText(requireContext(), "Didn't found any app to take photo", Toast.LENGTH_SHORT).show()
+        }
     }
 
+    private fun updateInterfaceWithLocationData(latitude: Double, longitude: Double) {
+        val locationText = "Latitud: $latitude, Longitud: $longitude"
+        binding.imageData.text = locationText
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(requireActivity().packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val extras: Bundle? = data?.extras
+            val imageBitmap = extras?.get("data") as? Bitmap
+            // Muestra la vista previa en tu interfaz
+
+            // Guarda la foto (puedes personalizar esto según tus necesidades)
+            saveImageToGallery(imageBitmap)
+            showToast("Foto guardada")
+        }
+    }
+
+    private fun saveImageToGallery(imageBitmap: Bitmap?) {
+        TODO("Not yet implemented")
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
 
 
 }
